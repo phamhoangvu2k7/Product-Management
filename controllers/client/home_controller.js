05d826c733c94cf56ea4ac95ae87a8bec2090c20
Pham Hoang Vu <phamvuhoang486@gmail.com>
2025-12-09 14:31:58 +0700

    update: show new product

--- controllers/client/home_controller.js
+++ controllers/client/home_controller.js
@@ -4,17 +4,25 @@ const priceProductHelpers = require("../../helpers/priceProduct");
 
 // [GET] /
 module.exports.index = async (req, res) => {
+    // Get featured product
     let find = {
         featured: "1",
         deleted: false,
         status: "active"
     }
     const productsFeatured = await Product.find(find);
+    const newProductsFeatured = priceProductHelpers.priceProduct(productsFeatured);
 
-    const newProducts = priceProductHelpers.priceProduct(productsFeatured);
+    // Get new product
+    const prodcutdNew = await Product.find({
+        deleted: false,
+        status: "active"
+    }).sort({position: "desc"});
+    const productsNew = priceProductHelpers.priceProduct(prodcutdNew);
 
     res.render("client/pages/home/index", {
         pageTitle: "Trang chủ",
-        productsFeatured: newProducts
+        productsFeatured: newProductsFeatured,
+        productsNew: productsNew
     });
 }
