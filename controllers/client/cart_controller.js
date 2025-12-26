47386d52565dfcd5a62cef97f677f4d9a85ba6f7
Pham Hoang Vu <phamvuhoang486@gmail.com>
2025-12-26 13:11:04 +0700

    update: page of cart

--- controllers/client/cart_controller.js
+++ controllers/client/cart_controller.js
@@ -1,4 +1,36 @@
 const Cart = require("../../models/carts_model");
+const Product = require("../../models/product_model");
+
+// [GET] /cart
+module.exports.index = async (req, res) => {
+    const cartId = req.cookies.cartId;
+
+    const cart = await Cart.findOne({
+        _id: cartId
+    });
+
+    if (cart.products.length > 0) {
+        for (const item of cart.products) {
+            const productId = item.product_id;
+            const productInfo = await Product.findOne({
+                _id: productId,
+                deleted: false
+            }).select("title thumbnail slug price discountPercentage");
+            
+            item.productInfo = productInfo;
+            item.totalPrice = (item.productInfo.price - parseInt(item.productInfo.price * item.productInfo.discountPercentage / 100)) * item.quantity;
+        }
+    }
+
+    cart.totalPrice = cart.products.reduce((sum, item) => sum + item.totalPrice, 0);
+
+    console.log(cart);
+
+    res.render("client/pages/cart/index", {
+        pageTitle: "Giỏ hàng",
+        cartDetail: cart
+    });
+}
 
 // [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
@@ -31,11 +63,11 @@ module.exports.addPost = async (req, res) => {
             product_id: productId,
             quantity: quantity
         };
-    
+
         await Cart.updateOne(
-            { 
-                _id: cartId 
-            }, 
+            {
+                _id: cartId
+            },
             {
                 $push: { products: objectCart }
             }
